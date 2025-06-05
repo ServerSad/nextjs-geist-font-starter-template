eafb7cf9eb851e407d8aa2742f85f421d2dab8c4
ServerSad <129608240+ServerSad@users.noreply.github.com>
2025-06-04 23:21:21 -0300

    Add .next/static/chunks/src_app_welcome_page_tsx_4aae0543._.js 1749090079838

---
+++ .next/static/chunks/src_app_welcome_page_tsx_4aae0543._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_framer-motion_dist_es_24e93cb0._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_fa3ea29e._.js",
+  "static/chunks/node_modules_b7cf6d40._.js",
+  "static/chunks/src_c83786f5._.js"
+],
+    source: "dynamic"
+});
