990e3e282e5983b16ccbb05e97aeca4c98345783
ServerSad <129608240+ServerSad@users.noreply.github.com>
2025-06-04 23:21:22 -0300

    Add .next/static/chunks/src_app_[username]_page_tsx_4aae0543._.js 1749090080941

---
+++ .next/static/chunks/src_app_[username]_page_tsx_4aae0543._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_bc29b052._.js",
+  "static/chunks/src_cec1dc01._.js"
+],
+    source: "dynamic"
+});
