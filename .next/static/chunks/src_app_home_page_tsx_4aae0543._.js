664ce82f469f2b87fdbad33ba2a5a612e9dca7bf
ServerSad <129608240+ServerSad@users.noreply.github.com>
2025-06-04 23:21:20 -0300

    Add .next/static/chunks/src_app_home_page_tsx_4aae0543._.js 1749090078815

---
+++ .next/static/chunks/src_app_home_page_tsx_4aae0543._.js
@@ -0,0 +1,16 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_c2ca3f6a._.js",
+  "static/chunks/node_modules_lucide-react_dist_esm_icons_index_208f44ab.js",
+  "static/chunks/node_modules_lucide-react_dist_esm_icons_index_d4b74329.js",
+  "static/chunks/node_modules_lucide-react_dist_esm_icons_2fe48e59._.js",
+  "static/chunks/node_modules_lucide-react_dist_esm_lucide-react_a644e23c.js",
+  "static/chunks/node_modules_lucide-react_dist_esm_lucide-react_e8a320fa.js",
+  "static/chunks/node_modules_lucide-react_dist_esm_lucide-react_f0b2f71f.js",
+  "static/chunks/node_modules_framer-motion_dist_es_24e93cb0._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_fa3ea29e._.js",
+  "static/chunks/node_modules_efec4fb2._.js"
+],
+    source: "dynamic"
+});
